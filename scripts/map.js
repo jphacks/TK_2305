ad48e2b648d21b79f51c4710e8efe3425bb8af81
XYJ080 <101054474+JY251@users.noreply.github.com>
2023-10-28 14:39:02 +0900

    update to my latest version

--- scripts/map.js
+++ scripts/map.js
@@ -1,5 +1,5 @@
 // Initialize the map
-const map = L.map("map").setView([0, 0], 15);
+var map = L.map("map").setView([0, 0], 15);
 
 // Add OpenStreetMap tile layer
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -12,8 +12,9 @@ const data = [
       latitude: 35.6895,
       longitude: 139.6917,
     },
-    forgotten_items: ["鍵", "携帯電話"],
+    forgotten_item: ["携帯電話"],
     user_name: "ジョン・ドウ",
+    rewards: "¥1000",
     required_time: "2 時間",
     start_time: "3:30 PM",
     end_time: "5:30 PM",
@@ -23,7 +24,7 @@ const data = [
       latitude: 35.6575,
       longitude: 139.7027,
     },
-    forgotten_items: ["財布", "傘"],
+    forgotten_item: ["傘"],
     user_name: "ジェーン・スミス",
     required_time: "3 時間",
     start_time: "10:00 AM",
@@ -34,7 +35,7 @@ const data = [
       latitude: 35.6895,
       longitude: 139.6921,
     },
-    forgotten_items: ["メガネ", "本"],
+    forgotten_item: ["メガネ"],
     user_name: "アリス・ジョンソン",
     required_time: "1.5 時間",
     start_time: "1:30 PM",
@@ -43,18 +44,34 @@ const data = [
 ];
 
 data.forEach((locationData) => {
+  console.log(locationData); // for debug
   const latitude = locationData.pin_location.latitude;
   const longitude = locationData.pin_location.longitude;
-  const forgottenItems = locationData.forgotten_items.join(", "); // Join items into a comma-separated string
+  const forgottenItems = locationData.forgotten_item.join(", "); // Join items into a comma-separated string
 
   // Create a marker on the map
   const marker = L.marker([latitude, longitude]).addTo(map);
 
+    //   // Create a custom icon for the marker
+    // const customIcon = L.icon({
+    //   iconUrl: 'path-to-your-custom-image.png', // Replace with the actual path to your image
+    //   iconSize: [32, 32], // Set the width and height of the icon in pixels
+    //   iconAnchor: [16, 32], // Adjust the anchor point if needed
+    // });
+
+    // // Create a marker using the custom icon
+    // const marker = L.marker([latitude, longitude], { icon: customIcon }).addTo(map);
+
+
   // Create a popup for the marker with the desired information
   marker.bindPopup(
-    `<div class="green-popup">忘れ物: ${forgottenItems}<br>ユーザー名: ${locationData["user_name"]}<br>必要な時間: ${locationData["required_time"]}<br>使い始める時間: ${locationData["start_time"]}<br>使い終わる予定の時間: ${locationData["end_time"]}</div>`,
-    {class : "green-popup"}
+    `<div class="custom-title">${forgottenItems}</div><br>ユーザー名: ${locationData["user_name"]}<br>報酬: ${locationData["rewards"]}<br>必要な時間: ${locationData["required_time"]}<br>使い始める時間: ${locationData["start_time"]}<br>使い終わる予定の時間: ${locationData["end_time"]}`,
   );
+
+  //
+  // marker.bindPopup(
+  //   `忘れ物: ${forgottenItems}<br>ユーザー名: ${locationData["user_name"]}<br>必要な時間: ${locationData["required_time"]}<br>使い始める時間: ${locationData["start_time"]}<br>使い終わる予定の時間: ${locationData["end_time"]}`,
+  // );
 
   // Open the popup by default
   marker.openPopup();
