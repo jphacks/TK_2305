a4e9b0ba2062fd9c4c7554d66a97393cee7e7487
XYJ080 <101054474+JY251@users.noreply.github.com>
2023-10-28 12:25:15 +0900

    <IMP> (Succ) Display map (only map) initial commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,7 @@
-function sayHelloWorld () {
-    alert('Hello World')
-}
+// Initialize the map
+var map = L.map('map').setView([0, 0], 15);
+
+// Add OpenStreetMap tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 100,
+}).addTo(map);
