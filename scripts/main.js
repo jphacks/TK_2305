5bc110d2b2a46fae8ecdc405c1abc8167379b8a5
XYJ080 <101054474+JY251@users.noreply.github.com>
2023-10-28 14:09:26 +0900

    added code to change the pin image (not used yet)

--- scripts/main.js
+++ scripts/main.js
@@ -52,6 +52,17 @@ data.forEach((locationData) => {
   // Create a marker on the map
   const marker = L.marker([latitude, longitude]).addTo(map);
 
+    //   // Create a custom icon for the marker
+    // const customIcon = L.icon({
+    //   iconUrl: 'path-to-your-custom-image.png', // Replace with the actual path to your image
+    //   iconSize: [32, 32], // Set the width and height of the icon in pixels
+    //   iconAnchor: [16, 32], // Adjust the anchor point if needed
+    // });
+
+    // // Create a marker using the custom icon
+    // const marker = L.marker([latitude, longitude], { icon: customIcon }).addTo(map);
+
+
   // Create a popup for the marker with the desired information
   marker.bindPopup(
     `<div class="custom-title">${forgottenItems}</div><br>ユーザー名: ${locationData["user_name"]}<br>報酬: ${locationData["rewards"]}<br>必要な時間: ${locationData["required_time"]}<br>使い始める時間: ${locationData["start_time"]}<br>使い終わる予定の時間: ${locationData["end_time"]}`,
