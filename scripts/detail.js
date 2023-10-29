1b0025b54883a9396f74ece6679f19aa4eb6b101
niwatoro <girlwithskyhairpin@gmail.com>
2023-10-29 11:45:43 +0900

    Add user name to detail view

--- scripts/detail.js
+++ scripts/detail.js
@@ -1,4 +1,4 @@
-import { getPinById } from "./firestore.js";
+import { getPinById, getUserById } from "./firestore.js";
 
 // fetch data by query parameter id
 
@@ -6,11 +6,13 @@ const url = new URL(window.location.href);
 const id = url.searchParams.get("id");
 
 getPinById(id).then((pin) => {
-  const data = pin.data;
-  const deadline = data.deadline.toDate();
+  getUserById(pin.data.user_id).then((user) => {
+    const data = pin.data;
+    const deadline = data.deadline.toDate();
 
-  document.getElementById("forgotten-item").innerText = data.forgotten_item;
-  document.getElementById("user-name").innerText = data.user_name;
-  document.getElementById("reward").innerText = data.reward;
-  document.getElementById("deadline").innerText = deadline;
+    document.getElementById("forgotten-item").innerText = data.forgotten_item;
+    document.getElementById("user-name").innerText = user.data.user_name;
+    document.getElementById("reward").innerText = data.reward;
+    document.getElementById("deadline").innerText = deadline;
+  });
 });
